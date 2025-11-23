003ff8202ef3a8b6be1ae2b499cb30985e8c5558
hwang241101 <hwang241101@gmail.com>
2025-11-23 18:14:29 +0900

    feat(env): comminlint Setting

---
+++ commonlint.config.js
@@ -0,0 +1,11 @@
+import { rules } from "eslint-config-airbnb-base";
+
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["feat", "fix", "docs"]],
+    "scope-enum": [2, "always", ["ui", "logic", "env"]],
+    "scope-empty": [2, "never"],
+    "subject-empty": [2, "never"],
+  },
+};
